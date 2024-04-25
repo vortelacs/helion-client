9b5337fd329dafba39b48ecb00887997ed0da022
Evghenii <evghenii.sirbu@yahoo.com>
2024-04-25 08:34:43 +0300

    added pdf download support

--- src/pages/ProcessTable.tsx
+++ src/pages/ProcessTable.tsx
@@ -18,6 +18,31 @@ const ProcessTable = () => {
       });
   };
 
+  const handleDownloadProcess = (id: number) => {
+    axios({
+      url: `http://localhost:5179/process/pdf/${id}`,
+      method: "GET",
+      responseType: "blob",
+    })
+      .then((response) => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", `process_${id}.pdf`);
+        document.body.appendChild(link);
+        link.click();
+        link.parentNode?.removeChild(link);
+        window.URL.revokeObjectURL(url);
+        console.log("Process downloaded successfully");
+      })
+      .catch((error) => {
+        console.error(
+          "Failed to download process:",
+          error.response ? error.response.data : "Unknown error"
+        );
+      });
+  };
+
   useEffect(() => {
     axios
       .get<ProcessInfoDTO[]>("http://localhost:5179/processes")
@@ -28,7 +53,7 @@ const ProcessTable = () => {
   }, [deleteCount]);
 
   return (
-    <div className="relative overflow-x-auto">
+    <div className="lg:my-2 relative overflow-x-auto">
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
@@ -37,6 +62,7 @@ const ProcessTable = () => {
             <th className="px-4 py-3">Company</th>
             <th className="px-4 py-3">Services</th>
             <th className="px-4 py-3">GPS Location</th>
+            <th className="px-4 py-3">Download PDF</th>
             <th className="px-4 py-3">Delete process</th>
           </tr>
         </thead>
@@ -54,7 +80,15 @@ const ProcessTable = () => {
               <td className="px-4 py-3 border">{process.gpsLocation}</td>
               <td className="px-4 py-3 border">
                 <button
-                  className="bg-red-300 hover:bg-red-400 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+                  className="bg-blue-400 hover:bg-blue-500 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+                  onClick={() => handleDownloadProcess(process.id)}
+                >
+                  Download PDF
+                </button>
+              </td>
+              <td className="px-4 py-3 border">
+                <button
+                  className="bg-red-500 hover:bg-red-600 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline"
                   onClick={() => handleDeleteProcess(process.id)}
                 >
                   Delete
